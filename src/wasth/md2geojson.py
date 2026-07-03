3685e5ec428e987fdc84b58bbd12d0032ff20bfb
Pedro P. Palazzo <palazzo@unb.br>
2026-07-03 18:15:48 -0300

    Classe para geojson.Feature, não testada

--- src/wasth/md2geojson.py
+++ src/wasth/md2geojson.py
@@ -1,14 +1,16 @@
 """Converte fichas em Markdown+YAML para geoJSON
 
 Usa frontmatter para extrair metadados.
+Não temos previsão de implementar o caminho inverso
+(geoJSON para fichas em Markdown+YAML).
 """
 
 import sys
 import os
 import frontmatter
-import json
+import geojson
 import geopandas as gpd
-import wasth.normalize
+import wasth.normalize as norm
 
 def filelist(input) -> list | None:
     if len(sys.argv) > 1:
@@ -30,25 +32,62 @@ Informar um caminho relativo de pasta ou nomes de arquivos/ficheiros:
         print("Operação cancelada")
     return filelist
 
-def frontmatter(file, enc='utf-8') -> dict:
-    with open(file, 'r', encoding=enc) as f:
-        post = frontmatter.load(f)
-    return post['metadata']
+class GeoFeature:
+    def __init__(self, input_path: str, encoding='utf-8') -> geojson.Feature:
+        self.inp = input_path
+        self.enc = encoding
+        with open(self.inp, 'r', encoding=self.enc) as f:
+            post = frontmatter.load(f)
+        normalized = norm.NormalizedWork(post)
+        frontmatter = normalized['metadata']
+        for place in frontmatter['spatial']:
+            if place['type'] == 'site':
+                lon = place['type']['location']['lon']
+                lat = place['type']['location']['lat']
+                break
+        point = geojson.Point((lon, lat))
+        props = {}
+        props['name'] = frontmatter['title']
+        props['teaser'] = frontmatter['header']['teaser']
+        props['id'] = frontmatter['id']
+        feature = geojson.feature(geometry=point, properties=props)
+        return feature
 
-def md2geojson(dict) -> dict:
-    pass
+    def valid_geojson(feature) -> None:
+        return geojson.is_valid(feature)
 
-def f_write(dict) -> None:
-    pass
+def collect_features(features: list) -> geojson.FeatureCollection | None:
+    collection = geojson.FeatureCollection(features)
+    return collection
+
+def f_write(collection: geojson.FeatureCollection | None, dir, filename) -> None:
+    try:
+        os.makedirs(dir)
+        print(f"📁  Pasta '{dir}' criada com sucesso.")
+        dest = os.path.join(dir, filename)
+    except FileExistsError:
+        print(f"📁  Pasta '{dir}' já existe.")
+    except PermissionError:
+        print(f"❌  Não foi possível criar a pasta '{dir}': sem permissões.")
+    except Exception as e:
+        print(f"❌  Erro na criação da pasta: {e}")
+    try:
+        with open(dest, 'w') as f:
+            geojson.dump(collection, f)
+        print(f"📄  Arquivo '{dest}' gravado com sucesso.")
+    except Exception as e:
+        print(f"❌  Erro na escrita do arquivo '{dest}': {e}")
 
 def main(args: list[str] | None = None) -> int:
     if args is None:
         args = sys.argv
     files = filelist(args)
-    posts = []
     for f in files:
-        posts.append(frontmatter(f))
-    f_write(md2geojson(posts))
+        feature = GeoFeature(f)
+        valid_geojson = feature.valid_geojson(feature)
+        features.append(valid_geojson)
+    collection = collect_features(features)
+    f_write(geojson.dumps(collection), sort_keys=True)
 
 if __name__ == "__main__":
     raise SystemExit(main())
